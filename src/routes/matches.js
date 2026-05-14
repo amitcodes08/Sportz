fc3fa90c4b7c82357eeb3a62407d0451ee5917b6
Amit Gupta <aryaamit387@gmail.com>
2026-05-14 10:29:24 +0530

    implement match api

---
+++ src/routes/matches.js
@@ -0,0 +1,48 @@
+import {Router} from 'express';
+import { createMatchSchema, listMatchesQuerySchema } from '../validation/matches.js';
+import { db } from '../db/db.js';
+import getMatchStatus from '../utils/match-status.js';
+import { matches } from '../db/schema.js';
+
+export const matchRouter = Router();
+
+matchRouter.get('/', async (req, res) => {
+  const parsed = listMatchesQuerySchema.safeParse(req.query);
+
+  if(!parsed.success) {
+    return res.status(400).json({ error: "Invalid query parameters" , details: JSON.stringify(parsed.error.errors)});
+  }
+
+  const limit = Math.min(parsed.data.limit ?? 50, MAX_LIMIT);
+
+  try {
+    const data = await db.select().from(matches).limit(limit).orderBy(matches.createdAt, 'desc');
+    res.json({ data });
+  } catch(e) {
+    res.status(500).json({ error: 'Failed to fetch matches', details: JSON.stringify(e) });
+  }
+});
+
+matchRouter.post('/', async(req, res) => {
+  const parsed = createMatchSchema.safeParse(req.body);
+
+  const {data : {startTime, endTime, homeScore, awayScore}} = parsed.success ? parsed : {data: {}};
+  if (!parsed.success) {
+    return res.status(400).json({ error: "Invalid request body" , details: JSON.stringify(parsed.error.errors)});
+  }
+
+  try {
+    const [event] = await db.insert(matches).values({
+      ...parsed.data,
+      startTime: new Date(startTime),
+      endTime: new Date(endTime),
+      homeScore: homeScore ?? 0,
+      awayScore: awayScore ?? 0,
+      status: getMatchStatus(new Date(startTime), new Date(endTime)),
+    }).returning();
+
+    res.status(201).json({ message: 'Match created successfully', data: event });
+  } catch(e) {
+    res.status(500).json({ error: 'Failed to create match', details: JSON.stringify(e) });
+  }
+});
